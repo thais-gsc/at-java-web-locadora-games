04c9ab0066bdb1cc7ab2a5a0facaff197c294d60
Thaís <thais.schontag@al.infnet.edu.br>
2021-06-06 15:11:49 -0300

    controller

--- src/main/java/br/edu/infnet/springmvc/controller/LocadoraController.java
+++ src/main/java/br/edu/infnet/springmvc/controller/LocadoraController.java
@@ -1,7 +1,9 @@
 package br.edu.infnet.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,5 +27,10 @@ public class LocadoraController {
 		model.addAttribute("titulo", game.getTitulo());
 
 		return "resultado";
+	}
+
+	@GetMapping("/login")
+	public String login(Model model) {
+		return "login";
 	}
 }
